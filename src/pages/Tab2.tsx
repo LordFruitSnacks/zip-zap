8da503f017f64c171096d850984367904070be45
Surya <surya.cks@gmail.com>
2023-06-23 19:15:59 -0500

    tried adding blocks for each thing

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonItem, IonBadge, IonLabel, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import csv from 'csv-parser';
+import fs from 'fs';
+import { useState, useEffect } from 'react';
 
 const Tab2: React.FC = () => {
+  const numberOfCards = 10;
+  const ionCards = []
+  for (let i = 0; i < numberOfCards; i++) {
+    ionCards.push(
+      <IonCard key={i}>
+        <IonCardHeader className = "card-header">
+          <IonCardTitle>Name</IonCardTitle>
+          <IonCardSubtitle>Description</IonCardSubtitle>
+          <IonBadge color="danger" className = "badge" id = "price-badge">Price</IonBadge> 
+          <IonBadge color="success" className = "badge" id= "topics-badge">Topics</IonBadge> 
+        </IonCardHeader>
+        <IonCardContent>
+          idk. put something about what the program's about or some description 
+        </IonCardContent>
+      </IonCard>
+      
+    );
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -10,15 +31,12 @@ const Tab2: React.FC = () => {
           <IonTitle>Jobs</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Jobs</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Jobs page" />
+      <IonContent>
+        {ionCards}
       </IonContent>
     </IonPage>
+    
+
   );
 };
 
